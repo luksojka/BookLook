9f1d62c185b07f4bfd7281a03c74df3eface0c6b
Łukasz Sójka <lsojka93@gmail.com>
2023-01-21 12:14:23 +0100

    REST POST addBook

--- src/main/java/com/booklook/booklook/catalog/web/CatalogController.java
+++ src/main/java/com/booklook/booklook/catalog/web/CatalogController.java
@@ -1,11 +1,15 @@
 package com.booklook.booklook.catalog.web;
 
 import com.booklook.booklook.catalog.application.port.CatalogUseCase;
+import com.booklook.booklook.catalog.application.port.CatalogUseCase.CreateBookCommand;
 import com.booklook.booklook.catalog.domain.Book;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,7 +31,7 @@ class CatalogController {
         } else if (author.isPresent()) {
             return catalog.findByAuthor(author.get());
         }
-            return catalog.findAll();
+        return catalog.findAll();
     }
 
     @GetMapping("/{id}")
@@ -36,5 +40,24 @@ class CatalogController {
                 .findById(id)
                 .map(book -> ResponseEntity.ok(book))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book addBook(@RequestBody RestCreateBookCommand command) {
+        Book book = catalog.addBook(command.toCommand());
+        return book;
+    }
+
+    @Data
+    private static class RestCreateBookCommand {
+        private String title;
+        private String author;
+        private Integer year;
+        private BigDecimal price;
+
+        CreateBookCommand toCommand() {
+            return new CreateBookCommand(title, author, year, price);
+        }
     }
 }
